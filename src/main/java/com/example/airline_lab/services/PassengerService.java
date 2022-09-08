0b767c118d84fe5008cfc37cd123b5c8433a8287
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-09-08 23:09:06 +0300

    setted up passenger methods

--- src/main/java/com/example/airline_lab/services/PassengerService.java
+++ src/main/java/com/example/airline_lab/services/PassengerService.java
@@ -1,13 +1,33 @@
 package com.example.airline_lab.services;
 
+import com.example.airline_lab.models.Passenger;
 import com.example.airline_lab.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PassengerService {
 
     @Autowired
     PassengerRepository passengerRepository;
+
+    // Add a new passenger
+    public Passenger addNewPassenger(Passenger passenger) {
+        passengerRepository.save(passenger);
+        return passenger;
+    }
+
+    // Display details of all passengers
+    public List<Passenger> getAllPassengers() {
+        return passengerRepository.findAll();
+    }
+
+    // Display details of a specific passenger
+    public Optional<Passenger> getPassengerById(Long id) {
+        return passengerRepository.findById(id);
+    }
 
 }
